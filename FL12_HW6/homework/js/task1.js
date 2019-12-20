a0379cea60f19b2b2d70df448b80d01fa32bc94a
straus <straus94@gmail.com>
2019-12-20 14:23:17 +0200

    fix eslint warnings

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,29 +2,31 @@
 let a = prompt('Please, enter A');
 let b = prompt('Please, enter B');
 let c = prompt('Please, enter C');
+const forSearchingDiscriminant = 4;
+const forSearchingX = 2;
 
-if (a == null || a == '' || b == null || b == '' || c == null || c == '') {
+if (a === null || a === '' || b === null || b === '' || c === null || c === '') {
     console.log('Invalid input data');
 } else {
     a = +a;
     b = +b;
     c = +c;
 
-    if (isNaN(a) || isNaN(b) || isNaN(c) || a == 0) {
+    if (isNaN(a) || isNaN(b) || isNaN(c) || a === 0) {
         console.log('Invalid input data');
     } else {
         let d, x, x1, x2;
-        d = (b * b) - (4 * a * c);
+        d = b * b - forSearchingDiscriminant * a * c;
         console.log('d = ' + d);
 
         if (d < 0) {
             console.log(`no solution (Discriminant < 0)`);
         } else if (d > 0) {
-            x1 = ((-b + Math.sqrt(d)) / (2 * a)).toFixed(2);
-            x2 = ((-b - Math.sqrt(d)) / (2 * a)).toFixed(2);
+            x1 = (-b + Math.sqrt(d)) / (a * forSearchingX);
+            x2 = (-b - Math.sqrt(d)) / (a * forSearchingX);
             console.log(`x1 = ${x1}, x2 = ${x2}`);
         } else {
-            x = (-b + Math.sqrt(d)) / (2 * a);
+            x = (-b + Math.sqrt(d)) / (a * forSearchingX);
             console.log(`x = ${x}`);
         }
     }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -3,12 +3,12 @@ let a = +prompt('Please, enter A');
 let b = +prompt('Please, enter B');
 let c = +prompt('Please, enter C');
 
-if (a == 0 || b == 0 || c == 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
+if (a === 0 || b === 0 || c === 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
     console.log('Triangle doesn’t exist');
-} else if ((a + b > c) || (b + c > a) || (a + c > b)) {
-    if (a + b == c * 2) {
+} else if (a + b > c || b + c > a || a + c > b) {
+    if (a + b === c + c) {
         console.log('Equilateral triangle');
-    } else if (a == b || a == c || b == c) {
+    } else if (a === b || a === c || b === c) {
         console.log('Isosceles triangle')
     } else {
         console.log('Scalene triangle')
