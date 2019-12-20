30cb9d858ee5c43fddf1b8a01da2fc08399c24a2
straus <straus94@gmail.com>
2019-12-20 11:16:03 +0200

    add task 2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,19 @@
 // Your code goes here
+let a = +prompt('Please, enter A');
+let b = +prompt('Please, enter B');
+let c = +prompt('Please, enter C');
+
+if (a == 0 || b == 0 || c == 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log('Triangle doesn’t exist');
+} else if ((a + b > c) || (b + c > a) || (a + c > b)) {
+    if (a + b == c * 2) {
+        console.log('Equilateral triangle');
+    } else if (a == b || a == c || b == c) {
+        console.log('Isosceles triangle')
+    } else {
+        console.log('Scalene triangle')
+    }    
+} else {
+    console.log('Triangle doesn’t exist');
+
+}
