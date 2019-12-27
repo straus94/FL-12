b3c78ef915140cb09b43659c5ca547a8ba7d1085
straus <straus94@gmail.com>
2019-12-27 16:39:34 +0200

    add task 1, and 90% task 2

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,75 @@
-// Your code goes here
+let password, checkPassword, newPassword;
+let mail = prompt('enter login');
+let userPass = 'UserPass';
+let adminPass = 'AdminPass';
+let cancel = 'Canceled';
+let goodLengthPassword = 6;
+let goodLengthMail = 5;
+console.log(mail);
+if (mail === '' || mail === null) {
+    alert(cancel);
+} else if (mail.length < goodLengthMail) {
+        alert(`I don't know any emails having name length less than 5 symbols`);
+} else if (mail === 'user@gmail.com') {
+        password = prompt('enter password');
+
+        if (password === '' || password === null) {
+            alert(cancel);
+        } else if (password === userPass) {
+            if (confirm('Do you want to change your password?')) {
+                checkPassword = prompt('write the old password');
+                if (checkPassword === '' || checkPassword === null) {
+                    alert(cancel);
+                } else if (checkPassword === userPass) {
+                    newPassword = prompt('enter new password');
+                    if (newPassword === '' || newPassword === null) {
+                        alert(cancel);
+                    } else if (newPassword.length < goodLengthPassword) {
+                        alert(`It’s too short password. Sorry.`);
+                    } else {
+                        if (prompt('again password') === newPassword) {
+                            alert(`You have successfully changed your password.`);
+                        } else {
+                            alert(`You wrote the wrong password.`);
+                        }
+                    }
+                }
+            } else {
+                alert('You have failed the change');
+            }
+        } else {
+            alert('Wrong password');
+        }
+} else if (mail === 'admin@gmail.com') {
+    password = prompt('enter password');
+
+    if (password === '' || password === null) {
+        alert(cancel);
+    } else if (password === adminPass) {
+        if (confirm('Do you want to change your password?')) {
+            checkPassword = prompt('write the old password');
+            if (checkPassword === '' || checkPassword === null) {
+                alert(cancel);
+            } else if (checkPassword === adminPass) {
+                newPassword = prompt('enter new password');
+                if (newPassword === '' || newPassword === null) {
+                    alert(cancel);
+                } else if (newPassword.length < goodLengthPassword) {
+                    alert(`It’s too short password. Sorry.`);
+                } else {
+                    if (prompt('again password') === newPassword) {
+                        alert(`You have successfully changed your password.`);
+                    } else {
+                        alert(`You wrote the wrong password.`);
+                    }
+                }
+            }
+        } else {
+            alert('You have failed the change');
+        }
+    } else {
+        alert('Wrong password');
+    }
+} else {
+    alert(`I don’t know you`);
+}
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,62 @@
-// Your code goes here
+let game = true;
+while (game) {
+    if (confirm(`Do you want to play a game?`)) {
+        let first = 100;
+        let second = 50;
+        let third = 25;
+        let loose = 0;
+        let winner = 0;
+        let range = 9;
+        let rand;
+        let level = 1;
+        let numberPocket, currentAttempht;
+        while (game) {
+            rand = parseInt(Math.random() * range);
+            for (let i = 3; i >= 0; i--) {
+                if (i === 3) {
+                    currentAttempht = first;
+                } 
+                if (i === 2) {
+                    currentAttempht = second;
+                } 
+                if (i === 1) {
+                    currentAttempht = third;
+                } 
+                let strAttemphtLeft = `Attempts left: ${i}`;
+                let strTotalPrice = `Total price: ${winner}$`;
+                let strPossiblePrice = `Possible price on current attempth: ${currentAttempht * level}$`;
+                numberPocket = +prompt(`Choose a roulette pocket number from 0 to ${range - 1}
+${strAttemphtLeft}
+${strTotalPrice}
+${strPossiblePrice}`);
+                if (numberPocket === rand) {
+                    if (confirm(`Congratulation, you won! Your prize is: ${winner += currentAttempht * level}$. Do you want to continue?`)) {
+                        range += 4;
+                        level++;
+                        game = true;
+                        break;
+                    } else {
+                        alert(`Thank you for your participation. Your prize is: ${winner}$`);
+                        game = false;
+                        break;
+                    }
+                } else if (i === 1) {
+                    alert(`Thank you for your participation. Your prize is ${loose}$`);
+                    if (confirm('You wants to play again?')) {
+                        level = 1;
+                        range = 9;
+                        winner = 0;
+                        break;
+                    } else {
+                        game = false;
+                        break;
+                    }
+                }
+            }
+        }
+    } else {
+        alert(`You did not become a billionaire, but can.`);
+        game = false;
+        break;
+    }
+}
